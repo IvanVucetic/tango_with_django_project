6d3c47848c3f0c2f1344fcff53e9935f5fdf1a48
Ivan Vucetic <vuceta87@yahoo.com>
2016-01-27 11:34:32 +0100

    comment out jQuery code

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -6,3 +6,9 @@ $("#likes").click(function () {
         $("#likes").hide();
     });
 });
+
+// When clicked, it will extract the category id from the button element, 
+// and then make an AJAX GET request which will make a call to /rango/like_category/ 
+// encoding the category_id in the request. If the request is successful, then 
+// the HTML element with id like_count (i.e. the <strong> ) is updated with 
+// the data returned by the request, and the HTML element with id likes (i.e. the <button>) is hidden.
